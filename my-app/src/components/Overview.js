b99920bf07fb36bcfe9b99b01fc92709fdecaf56
onifernando1 <onifernando1@gmail.com>
2023-06-01 21:01:15 +0100

    fix highScore typo

--- my-app/src/components/Overview.js
+++ my-app/src/components/Overview.js
@@ -1,57 +1,86 @@
-import React from "react";
 import Card from "./Card";
-import { useState } from 'react';
+import React, { useState, useEffect } from "react";
 import Scoreboard from "./Scoreboard";
 
-
-
 function Overview() {
-  const [allSelected, setAllSelected] =useState(false)
+  const [allSelected, setAllSelected] = useState(false);
 
-  const [score,setScore] = useState(0)
+  const [score, setScore] = useState(0);
 
-  const [previouslySelected, setPreviouslySelected] = useState([])
+  const [previouslySelected, setPreviouslySelected] = useState([]);
 
+  const [highScore, setHighScore] = useState(0);
 
-  let cards =["ب","ج","د","ه","و","ز","ح","ط","ي","ك","ل","م","ن","س","ع","ف"]
-  
+  let cards = [
+    "ب",
+    "ج",
+    "د",
+    "ه",
+    "و",
+    "ز",
+    "ح",
+    "ط",
+    "ي",
+    "ك",
+    "ل",
+    "م",
+    "ن",
+    "س",
+    "ع",
+    "ف",
+  ];
 
   const generateRandomNumber = (max) => {
     let answer = Math.floor(Math.random() * max);
     return answer;
   };
 
-
   const shuffleCards = (cards) => {
     for (let i = 0; i < cards.length; i++) {
-    let randomIndex = generateRandomNumber(cards.length);
-    let original = cards[i];
-    cards[i] = cards[randomIndex];
-    cards[randomIndex] = original;
+      let randomIndex = generateRandomNumber(cards.length);
+      let original = cards[i];
+      cards[i] = cards[randomIndex];
+      cards[randomIndex] = original;
     }
   };
-  
-  const selectCard = (card)=>{
-   if (previouslySelected.includes(card) == false) {
-    setPreviouslySelected(previouslySelected=>[...previouslySelected, card])
-    setScore(score + 1 )
-    console.log(previouslySelected)
-   } else {
-    setAllSelected(true)
-    alert("OH NO!")
-   }
-  }
-  
+
+  const updateHighScore = () => {
+    setHighScore(score);
+  };
+
+  const selectCard = (card) => {
+    if (previouslySelected.includes(card) == false) {
+      setPreviouslySelected((previouslySelected) => [
+        ...previouslySelected,
+        card,
+      ]);
+      setScore(score + 1);
+      console.log(previouslySelected);
+    } else {
+      setAllSelected(true);
+      updateHighScore();
+      alert(`Highscore:${highScore}`);
+    }
+  };
 
   shuffleCards(cards);
 
   return (
     <div>
-      <Scoreboard score={score}/>
+      <Scoreboard score={score} highScore={highScore} />
       <div className="overview">
-      {cards.map(card =>{
-        return <div onClick = {()=>{selectCard(card)}} className="card"><Card name = {card}/></div>
-      })}
+        {cards.map((card) => {
+          return (
+            <div
+              onClick={() => {
+                selectCard(card);
+              }}
+              className="card"
+            >
+              <Card name={card} />
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- my-app/src/components/Scoreboard.js
+++ my-app/src/components/Scoreboard.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function Scoreboard(props) {
-
-  return <div>{props.score}</div>
+  return (
+    <div>
+      <div> Score:{props.score}</div>
+      <div>Highscore: {props.highScore}</div>
+    </div>
+  );
 }
 
 export default Scoreboard;
